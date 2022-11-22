3761669387b4c81967ec9684f20f001a81e1f01e
DoctorCodeine <aimeeqnguyen@outlook.com>
2022-11-22 12:16:17 -0600

    webpack update
    Co-authored-by: Harvey Li <112590580+harvli@users.noreply.github.com>
    Co-authored-by: John Wroge <72668920+johnwroge@users.noreply.github.com>
    Co-authored-by: Aimée (Amy) Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
    Co-authored-by: Ed Cho <106274379+edcho720@users.noreply.github.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; //this is not rendering
+
+const App = () => {
+  return (
+    <div> 
+      <h1>ZEUS</h1>
+    </div>
+  )
+}
+
+export default App; 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,69 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = { 
-entry: './client/index.tsx',
-}
+module.exports = {
+	mode: 'development',
+
+	entry: './client/src/index.tsx',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.js',
+	},
+
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './client/src/index.tsx',
+		}),
+	],
+
+  devServer: {
+		host: 'localhost',
+		port: 8000,
+		// enable HMR on the devServer
+		hot: true,
+		// fallback to root for other urls
+		historyApiFallback: true,
+
+		static: {
+			// match the output path
+			directory: path.resolve(__dirname, 'dist'),
+			// match the output 'publicPath'
+			publicPath: '/',
+		},
+
+		headers: { 'Access-Control-Allow-Origin': '*' },
+	
+		proxy: {
+			'/api/**': {
+				target: 'http://localhost:3000/',
+				secure: false,
+			},
+		},
+	},
+
+
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+						plugins: [],
+					},
+				},
+			},
+			{
+				test: /\.js$/,
+				enforce: 'pre',
+				use: ['source-map-loader'],
+			},
+		],
+	},
+	resolve: {
+		// Enable importing JS / tSX files without specifying their extension
+		extensions: ['.js', '.jsx', '.tsx'],
+	},
+};
