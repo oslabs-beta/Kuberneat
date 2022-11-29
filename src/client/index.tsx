295ad7855f06e0bb9ee4af0d44f86cbed5658943
Aimee  Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
2022-11-29 00:30:21 -0600

    Revert "Ed/frontend"

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,28 +1,16 @@
-/* import * as React from 'react'; */
-// import {createRoot} from 'react-dom/client';
+import * as React from 'react';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import {App} from './App';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import "./index.css"; 
 
-import {ContextProvider} from "./Context" // import the ContextProvider so we can wrap it around
-
-// const container = document.getElementById('root') as HTMLElement;
-// let root = createRoot(container);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById('root') as HTMLElement;
+let root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
-    <ContextProvider>{/* wrap everything in the ContextProvider */}
-    <BrowserRouter>
-      {/* <App aProp="Hello from the App component!" /> */}
-      <App />
-    </BrowserRouter>
-    </ContextProvider>
-  // </React.StrictMode>
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
 );
 
 
