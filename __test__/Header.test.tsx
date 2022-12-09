aaac37e5ff7b59ab040f00110d64a3a784b3a6fa
harvli <hl.138@hotmail.com>
2022-12-09 17:57:44 -0500

    backend formatted
    Co-authored-by: Harvey Li <112590580+harvli@users.noreply.github.com>
    Co-authored-by: John Wroge <72668920+johnwroge@users.noreply.github.com>
    Co-authored-by: Aimée Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
    Co-authored-by: Ed Cho <106274379+edcho720@users.noreply.github.com>

--- __test__/Header.test.tsx
+++ __test__/Header.test.tsx
@@ -2,29 +2,26 @@
  * @jest-environment jsdom
  */
 
- test('use jsdom in this test file', () => {
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
-  });
+test('use jsdom in this test file', () => {
+	const element = document.createElement('div');
+	expect(element).not.toBeNull();
+});
 
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Header from '../src/client/components/Header';
 
-// can use DESCRIBE BLOCKS to nest mulitple tests that are similar 
+// can use DESCRIBE BLOCKS to nest mulitple tests that are similar
 // ie: all are for a component and some functionality for it
-describe("Header component testing", () => {
-
-    // what we are testing
-test('Header component should have a title', 
-async () => {
-    // 1st render the component we want to test
-    render(<Header title="I am the Header" subtitle="I am the Header's subtile" />);
-    // find an element we want to interact with
-    const divElement = screen.getByRole("header");
-    // use screen to interact with it via its methods
-    expect(divElement).toContain("I am the Header");
-    //Assertion that the results are as expected
-});
-
+describe('Header component testing', () => {
+	// what we are testing
+	test('Header component should have a title', async () => {
+		// 1st render the component we want to test
+		render(<Header title='I am the Header' subtitle="I am the Header's subtile" />);
+		// find an element we want to interact with
+		const divElement = screen.getByRole('header');
+		// use screen to interact with it via its methods
+		expect(divElement).toContain('I am the Header');
+		//Assertion that the results are as expected
+	});
 });
