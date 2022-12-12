4f49de6ed3b71b489a0f02b3d5aebc896397fb0d
DoctorCodeine <aimeeqnguyen@outlook.com>
2022-12-11 20:52:49 -0600

    Setting Cookie data for user
    Co-authored-by: Harvey Li <112590580+harvli@users.noreply.github.com>
    Co-authored-by: John Wroge <72668920+johnwroge@users.noreply.github.com>
    Co-authored-by: Amy Nguyen (Aimée) <106037255+DoctorCodeine@users.noreply.github.com>
    Co-authored-by: Ed Cho <106274379+edcho720@users.noreply.github.com>

--- src/server/controllers/cookieController.ts
+++ src/server/controllers/cookieController.ts
@@ -1,1 +1,13 @@
 //Set Cookies and Session
+import { Request, Response, NextFunction } from 'express';
+
+const cookieController: object = {
+	setUserCookie(res: Response<any>, req: Request<any>, next: NextFunction) {
+		//setting cookie
+		res.cookie('User', res.locals.foundUser, { httpOnly: true });
+		res.cookie('Password', res.locals.foundUser.password, { httpOnly: true });
+		return next();
+	},
+};
+
+module.exports = cookieController;
