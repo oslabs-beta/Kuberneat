c3db630ac968f285d0883e1403877d62eb747ec7
DoctorCodeine <aimeeqnguyen@outlook.com>
2022-12-13 12:05:01 -0600

    Fixed overflowing div

--- src/client/components/GrafanaDash.tsx
+++ src/client/components/GrafanaDash.tsx
@@ -1,34 +1,26 @@
 import * as React from 'react';
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { Context } from '../Context';
-import Header from './Header';
 
-import { Box } from '@mui/material';
+// import { Box } from '@mui/material';
 
 import { AppProps } from '../interfaces';
-import { ReactNode, ReactElement } from 'react';
+import {ReactElement } from 'react';
 
 function GrafanaDash(): ReactElement {
 	const { darkModeOn } = useContext<AppProps>(Context);
 
 	return (
-		<Box m="20px">
+		<div>
 			<div className={darkModeOn ? 'dash-dark' : 'dash-light'}>
-				<Header
-					path="/grafanadash"
-					title="Grafana Dashboard"
-					subtitle="Just one of the many ways to visualize your cluster"
-				/>
-
 				<iframe
 					src="http://localhost:3001/d/I1MUxS54k/final-dashboard?orgId=1&from=1670948876446&to=1670949776446"
 					frameBorder="0"
 					loading="eager"
-					title="Grafana Chart"
 					className={darkModeOn ? 'iframe-dark' : 'iframe-light'}
 				></iframe>
 			</div>
-		</Box>
+		</div>
 	);
 }
 
