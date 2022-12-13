7c61b603fcfca2ecd1ee62f4fbfe3c111e46c1c8
harvli <hl.138@hotmail.com>
2022-12-12 23:42:00 -0500

    extra dashboard with metrics from aimee
    Co-authored-by: Harvey Li <112590580+harvli@users.noreply.github.com>
    Co-authored-by: John Wroge <72668920+johnwroge@users.noreply.github.com>
    Co-authored-by: Aimée Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
    Co-authored-by: Ed Cho <106274379+edcho720@users.noreply.github.com>

--- src/client/components/GrafanaDash.tsx
+++ src/client/components/GrafanaDash.tsx
@@ -19,9 +19,9 @@ function GrafanaDash(): ReactElement {
 					title='OG Grafana Dashboard goes here'
 					subtitle='Just one of the many ways to visualize your cluster'
 				/>
-
+				{/* http://localhost:3001/d/0dsovdF4z/zeus?orgId=1&refresh=5s&kiosk=&from=1669924943773&to=1669925243773  */}
 				<iframe
-					src='http://localhost:3001/d/0dsovdF4z/zeus?orgId=1&refresh=5s&kiosk=&from=1669924943773&to=1669925243773'
+					src='http://localhost:3001/d/I1MUxS54k/final-dashboard?orgId=1&refresh=5s&kiosk=&from=1670905355669&to=1670906255669'
 					frameBorder='0'
 					loading='eager'
 					title='Grafana Chart'
