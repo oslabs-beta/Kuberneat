c06f1b862b40ef513eed7977aebdbbdad607c293
Aimee Nguyen <aimeeqnguyen@outlook.com>
2023-11-07 23:59:57 -0800

    Migerating to Next.js

---
+++ zeus-next-app/src/pages/_app.js
@@ -0,0 +1,9 @@
+// import './../app/globals.css';
+import '../styles/globals.css';
+import '../index.css';
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App;
