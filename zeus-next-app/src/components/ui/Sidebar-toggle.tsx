25acc6d1d69f626905a462cab638c02411c8843f
Aimee Nguyen <aimeeqnguyen@outlook.com>
2023-11-13 15:27:55 -0800

    New UI features

---
+++ zeus-next-app/src/components/ui/Sidebar-toggle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@nextui-org/react';
+
+const SidebarToggle = () => {
+  return ( 
+    <div className="text-center">
+    <Button color="secondary" variant="ghost">
+        Ghost
+      </Button>  
+</div>
+  )
+
+}
+export default SidebarToggle;
---
+++ zeus-next-app/src/components/ui/Sidebar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>sidebar</h1>
+      <ul>
+        <li>Home</li>
+        <li>Cluster Web</li>
+        <li>Pods</li>
+        <li>Promtheus</li>
+        <li>Alerts</li>
+      </ul>
+    </div>
+  )
+
+}
+
+export default Sidebar;
