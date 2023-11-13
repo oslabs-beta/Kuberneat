032e2a13cddc58e07d7b10942b3434c946cf9c4d
Aimee Nguyen <aimeeqnguyen@outlook.com>
2023-11-13 13:07:40 -0800

    added Header and dropdown feature on home page

--- zeus-next-app/src/components/MainNavigation.tsx
+++ zeus-next-app/src/components/MainNavigation.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { signOut } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
+import { NextUIProvider } from '@nextui-org/system';
+import DropdownFeature from './ui/DropdownFeature';
 
+interface Session {
+  user: {
+    name: string;
+  };
+}
 const MainNavigation = () => {
   const { data: session } = useSession();
-  const name = session?.user?.name;
+  const name = session?.user?.name|| "";
   return (
-    <header className="flex items-center justify-between p-4">
-          <h1 className="text-2xl font-bold">Welcome {name}</h1>  
-          <button className="flex justify-start items-center p-2 rounded-sm border-gray-300" onClick={()=>signOut()}>
-          </button>
+    <header className="flex items-center justify-between p-4 !bg-black">
+          <NextUIProvider>
+            <DropdownFeature name={name}/>
+          </NextUIProvider>
     </header>
   )
 };
